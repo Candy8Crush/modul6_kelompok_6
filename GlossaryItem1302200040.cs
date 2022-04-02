4130465f9f971ca112a0606759d30104bd724575
riannf28 <riannurfauzi@student.telkomuniversity.ac.id>
2022-04-02 17:29:46 +0700

    menambahkan class GlossaryItem1302200040

---
+++ GlossaryItem1302200040.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_kelompok_6
+{
+    class GlossaryItem1302200040
+    {
+    }
+}
--- Program.cs
+++ Program.cs
@@ -15,6 +15,10 @@ namespace modul6_kelompok_6
             string json2 = File.ReadAllText(@"C:\\Users\\user\\Downloads\\Rian\\Semester 4\\KPL\\PRAKTIKUM\\modul6_kelompok_6\\jurnal6_2_1302200040.json");
             DataMahasiswa1302200040 data2 = JsonSerializer.Deserialize<DataMahasiswa1302200040>(json2);
             Console.WriteLine(data2);
+
+            string json3 = File.ReadAllText(@"C:\\Users\\user\\Downloads\\Rian\\Semester 4\\KPL\\PRAKTIKUM\\modul6_kelompok_6\\jurnal6_3_1302200040.json");
+            DataMahasiswa1302200040 data3 = JsonSerializer.Deserialize<DataMahasiswa1302200040>(json3);
+            Console.WriteLine(data2);
         }
     }
 }
