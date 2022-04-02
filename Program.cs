1ee9963a8863f2eab44be31b8248535ee7a4b853
riannf28 <riannurfauzi@student.telkomuniversity.ac.id>
2022-04-02 17:27:40 +0700

    menambahkan class TeamMembers1302200040

--- Program.cs
+++ Program.cs
@@ -11,6 +11,10 @@ namespace modul6_kelompok_6
             string json = File.ReadAllText(@"C:\\Users\\user\\Downloads\\Rian\\Semester 4\\KPL\\PRAKTIKUM\\modul6_kelompok_6\\jurnal6_1_1302200040.json");
             DataMahasiswa1302200040 data = JsonSerializer.Deserialize<DataMahasiswa1302200040>(json);
             Console.WriteLine(data);
+
+            string json2 = File.ReadAllText(@"C:\\Users\\user\\Downloads\\Rian\\Semester 4\\KPL\\PRAKTIKUM\\modul6_kelompok_6\\jurnal6_2_1302200040.json");
+            DataMahasiswa1302200040 data2 = JsonSerializer.Deserialize<DataMahasiswa1302200040>(json2);
+            Console.WriteLine(data2);
         }
     }
 }
