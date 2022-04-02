4a0bff2840c2f18eb07facaeac0ccec5deb0469e
riannf28 <riannurfauzi@student.telkomuniversity.ac.id>
2022-04-02 17:17:20 +0700

    menambahkan class DataMahasiswa1302200040

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace modul6_kelompok_6
 {
@@ -6,7 +8,9 @@ namespace modul6_kelompok_6
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string json = File.ReadAllText(@"C:\\Users\\user\\Downloads\\Rian\\Semester 4\\KPL\\PRAKTIKUM\\modul6_kelompok_6\\jurnal6_1_1302200040.json");
+            DataMahasiswa1302200040 data = JsonSerializer.Deserialize<DataMahasiswa1302200040>(json);
+            Console.WriteLine(data);
         }
     }
 }
