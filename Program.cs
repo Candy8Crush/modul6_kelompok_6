63468347e470be76adf35ed2a69cb945917db69d
luthfi55 <luthfi12354@gmail.com>
2022-04-02 16:11:13 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace modul6_kelompok_6
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
