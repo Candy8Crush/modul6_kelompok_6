ff18f27fc2b7fcac3501c362a151ae1709c4ead9
Elia Angga <eliangga2002@gmail.com>
2022-04-02 18:28:33 +0700

    commit all

---
+++ DataMahasiswa_1302204005.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace modul6_kelompok_6
+{
+    public class DataMahasiswa_1302204005
+    {
+        public DataMahasiswa_1302204005()
+        {
+        }
+    }
+}
---
+++ TeamMembers_1302204005.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace modul6_kelompok_6
+{
+    public class TeamMembers
+    {
+        public TeamMembers()
+        {
+        }
+    }
+}
