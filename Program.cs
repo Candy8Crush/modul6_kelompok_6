5ba0d98083a8370edcb405422feb224d90a3d728
Boma Fira Suganda <100770086+BomaFira@users.noreply.github.com>
2022-04-02 19:02:03 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace modul6_kelompok_6
 {
-    class MainClass
+    internal class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DataMahasiswa_1302204036 d = new DataMahasiswa_1302204036();
+
+            d.ReadJSON();
+
+            Console.WriteLine();
+
+            TimMahasiswa_1302204036 s = new TimMahasiswa_1302204036();
+
+            s.ReadJSON();
+
         }
     }
 }
---
+++ TimMahasiswa_1302204036.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace modul6_kelompok_6
+{
+    class TimMahasiswa_1302204036
+    {
+        public TimMahasiswa[] mahasiswa { get; set; }
+
+        public void ReadJSON()
+        {
+            string fileName = "jurnal6_2_1302204036.json";
+
+            string jsonString = File.ReadAllText(fileName);
+
+            TimMahasiswa_1302204036 jsonData = JsonSerializer.Deserialize<TimMahasiswa_1302204036>(jsonString);
+
+            this.mahasiswa = jsonData.mahasiswa;
+
+            this.info();
+        }
+
+        public void info()
+        {
+            Console.WriteLine("List tim mahasiswa:");
+            foreach (var team in members)
+            {
+                team.info();
+            }
+        }
+    }
+
+    class TimMahasiswa
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public string nim { get; set; }
+
+        public void info()
+        {
+            Console.WriteLine(this.nim + " " + this.firstName + " " + this.lastName + " (" + this.age + " " + this.gender + ")");
+        }
+    }
+}
